544349808bf62fda7f2b2c8e3ddff1df05b31707
jac3rd <jac3rd@virginia.edu>
2020-03-06 19:48:45 -0500

    Initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Tilemap walkable, obstacles;
+    void Start() {
+        walkable = GameObject.Find("Walkable").GetComponent<Tilemap>();
+        obstacles = GameObject.Find("Obstacles").GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
